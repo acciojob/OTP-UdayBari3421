0c8e91e462257f3a7284f8fa0528a716f0dff75e
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-11-04 08:50:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function clickEvent(first,last){
+    if(first.value.length){
+      document.getElementById(last).focus();
+    }
+	if(last.value.length==0){
+		document.getElementById(first).focus();
+	}
+}
